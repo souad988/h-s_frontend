9e2b2cfc29b4e33dfde46468d5b9dbc574993e1f
souad988 <souadelmansouri88@gmail.com>
2023-11-20 12:44:57 +0100

    Add material ui and update authForm to use mui components

--- src/components/auth/AuthForm.jsx
+++ src/components/auth/AuthForm.jsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 import { withFormik } from 'formik';
+import {
+  Box, Typography, Container, CssBaseline, Avatar, TextField, Button, Grid, Link as MuiLink,
+} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { fetchLogin, fetchSignup } from '../../store/auth/authSlice';
+import '../../styles/index.css';
 
 const AuthForm = (props) => {
   const {
@@ -9,79 +17,168 @@ const AuthForm = (props) => {
     errors,
     handleChange,
     handleBlur,
-    handleSubmit,
   } = props;
+  const dispatch = useDispatch();
+  const { auth } = useSelector((state) => state);
   const location = useLocation();
   const [title, setTitle] = useState(location.pathname === '/login' ? 'Login' : 'Register');
   useEffect(() => {
     setTitle(location.pathname === '/login' ? 'Login' : 'Register');
   }, [location.pathname]);
+  const handleSubmit = (values) => {
+    console.log(values, auth);
+    if (title === 'Register') {
+      dispatch(fetchSignup({ user: values }));
+    } else {
+      dispatch(fetchLogin({ user: values }));
+    }
+  };
 
   return (
     <div>
-      <h1>{title}</h1>
-
-      <form onSubmit={handleSubmit}>
-        {title === 'Register'
-          ? (
-            <div className="input_block">
-              <input
-                type="text"
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            {title === 'Register' ? 'Sign Up' : 'Sign In'}
+          </Typography>
+        </Box>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          {title === 'Register' ? (
+            <Box>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="name"
+                label="User Name"
                 name="name"
-                placeholder="Enter your username"
+                autoComplete="name"
+                onBlur={handleBlur}
                 value={values.name}
                 onChange={handleChange}
+                autoFocus
               />
-              {errors.name && touched.name && <div id="error_message">{errors.name}</div>}
-            </div>
-          )
-          : null}
-        <div className="input_block">
-          <input
-            type="email"
+              {errors.name && touched.name && (<div id="name_error_message" className="error_message">{errors.name}</div>)}
+            </Box>
+          ) : null}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
             name="email"
-            placeholder="Enter email address"
+            autoComplete="email"
             value={values.email}
             onChange={handleChange}
+            autoFocus
           />
-          {errors.email && touched.email && <div id="feedback">{errors.email}</div>}
-        </div>
-        <div className="input_block">
-          <input
-            type="password"
+          {errors.email && touched.email && <div id="email_error_message" className="error_message">{errors.email}</div>}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
             name="password"
-            placeholder="Enter password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
             value={values.password}
             onChange={handleChange}
           />
-          {errors.password && touched.password && <div id="feedback">{errors.password}</div>}
-        </div>
-        {title === 'Register'
-          ? (
-            <div className="input_block">
-              <input
-                type="password"
+          {errors.password && touched.password && <div id="password_error_message" className="error_message">{errors.password}</div>}
+          {title === 'Register' ? (
+            <Box>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="confirmPassword"
+                label="User Name"
                 name="confirmPassword"
-                placeholder="Confirm password"
+                autoComplete="confirmPassword"
                 value={values.confirmPassword}
                 onChange={handleChange}
+                autoFocus
               />
-              {errors.confirmPassword && touched.confirmPassword && <div id="feedback">{errors.confirmPassword}</div>}
-            </div>
+              {errors.confirmPassword && touched.confirmPassword && <div id="confirmPassword_error_message" className="error_message">{errors.confirmPassword}</div>}
+            </Box>
+          ) : null}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            { title === 'Register' ? 'Sign Up' : 'Sign In'}
+          </Button>
+          {title === 'Register' ? (
+            <Grid container>
+              <Grid item xs>
+                <MuiLink href="/login" variant="body2">
+                  Already have an account? Sign in
+                </MuiLink>
+              </Grid>
+            </Grid>
           )
-          : null}
-        <div className="input_block">
-          <button type="submit">
-            {
-            title === 'Register' ? 'Register' : 'Login'
-          }
-          </button>
-        </div>
-
-        <Link to={title === 'Register' ? '/login' : '/signup'}>{title === 'Register' ? 'Login' : 'Register'}</Link>
-      </form>
+            : (
+              <Grid container>
+                <Grid item>
+                  <MuiLink href="#" variant="body2">
+                    Forgot password?
+                  </MuiLink>
+                </Grid>
+                <Grid item>
+                  <MuiLink href="/signup" variant="body2">
+                    Dont have an account? Sign Up
+                  </MuiLink>
+                </Grid>
+              </Grid>
+            )}
+        </Box>
+      </Container>
     </div>
   );
+};
+
+AuthForm.propTypes = {
+  values: PropTypes.shape({
+    name: PropTypes.string,
+    email: PropTypes.string,
+    password: PropTypes.string,
+    confirmPassword: PropTypes.string,
+    // Define the expected types for each property in the 'values' object
+    // These should align with the actual data structure in your application
+  }).isRequired,
+  touched: PropTypes.shape({
+    name: PropTypes.bool,
+    email: PropTypes.bool,
+    password: PropTypes.bool,
+    confirmPassword: PropTypes.bool,
+    // Define the expected types for each property in the 'touched' object
+    // These should align with the actual data structure in your application
+  }).isRequired,
+  errors: PropTypes.shape({
+    name: PropTypes.string,
+    email: PropTypes.string,
+    password: PropTypes.string,
+    confirmPassword: PropTypes.string,
+    // Define the expected types for each property in the 'errors' object
+    // These should align with the actual data structure in your application
+  }).isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleBlur: PropTypes.func.isRequired,
 };
 
 const AuthFormInhanced = withFormik({
@@ -113,14 +210,6 @@ const AuthFormInhanced = withFormik({
     }
     return errors;
   },
-
-  handleSubmit: (values, { setSubmitting }) => {
-    setTimeout(() => {
-      alert(JSON.stringify(values, null, 2));
-      setSubmitting(false);
-    }, 1000);
-  },
-
   displayName: 'BasicForm',
 })(AuthForm);
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@emotion/react';
 import './index.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
@@ -9,11 +10,14 @@ import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <ThemeProvider theme={{}}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ThemeProvider>
+  ,
 );
 
 reportWebVitals();
