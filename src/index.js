1e2bf9601ceff043916eb3d38addc9b827a207bf
Souad El Mansouri <souadelmansouri88@gmail.com>
2024-01-31 15:25:43 +0100

    Add exact root

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@emotion/react';
 import './index.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store/store';
@@ -11,10 +10,8 @@ import store from './store/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={{}}>
-    <Provider store={store}>
-      <BrowserRouter>
+    <Provider store={store}>    
         <App />
-      </BrowserRouter>
     </Provider>
   </ThemeProvider>
   ,
