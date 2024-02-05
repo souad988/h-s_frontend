165ef5f7172ae96682d5570b5e094ef949f28ff2
Algerina <kh.amelle@gmail.com>
2024-02-05 17:10:55 +0100

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginForm from './components/auth/Login';
-import SignUpForm from './components/auth/Signup';
-import ConfirmationError from './components/auth/Confirmation_error';
-import ResetPasswordLink from './components/auth/resetPasswordLink';
-import Home from './views/home';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginForm from "./components/auth/Login";
+import SignUpForm from "./components/auth/Signup";
+import ConfirmationError from "./components/auth/Confirmation_error";
+import ResetPasswordLink from "./components/auth/resetPasswordLink";
+import Home from "./views/home";
 
 const App = () => (
   <Router>
@@ -13,11 +13,59 @@ const App = () => (
       <Route path="/resetPassword" element={<ResetPasswordLink />} />
       <Route path="/signup" element={<SignUpForm />} />
       <Route path="/confirmation_error/:id" element={<ConfirmationError />} />
-      <Route path="/confirmation_success" element={<LoginForm confirmation="confirmation_success" />} />
-      <Route path="/confirmation_sent" element={<LoginForm confirmation="confirmation_sent" />} />
+      <Route
+        path="/confirmation_success"
+        element={<LoginForm confirmation="confirmation_success" />}
+      />
+      <Route
+        path="/confirmation_sent"
+        element={<LoginForm confirmation="confirmation_sent" />}
+      />
       <Route path="/" element={<Home />} exact />
     </Routes>
   </Router>
 );
 
+import "./App.css";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+// import LoginForm from "./components/auth/Login";
+// import SignUpForm from "./components/auth/Signup";
+// import ConfirmationError from "./components/auth/Confirmation_error";
+// import ResetPasswordLink from "./components/auth/resetPasswordLink";
+import Header1 from "./components/header/Header1";
+// import { Diversity1 } from "@mui/icons-material";
+
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header1 />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+
+    // <Router>
+
+    //   <Routes>
+    //     <Route path="/login" element={<LoginForm confirmation="" />} />
+    //     <Route path="/resetPassword" element={<ResetPasswordLink />} />
+    //     <Route path="/signup" element={<SignUpForm />} />
+    //     <Route path="/confirmation_error/:id" element={<ConfirmationError />} />
+    //     <Route
+    //       path="/confirmation_success"
+    //       element={<LoginForm confirmation="confirmation_success" />}
+    //     />
+    //     <Route
+    //       path="/confirmation_sent"
+    //       element={<LoginForm confirmation="confirmation_sent" />}
+    //     />
+    //     <Route path="/" element={<LoginForm confirmation="" />} exact />
+    //   </Routes>
+    // </Router>
+  );
+}
 export default App;
